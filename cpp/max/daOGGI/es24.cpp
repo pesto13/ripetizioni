e2c786623b976ff310e2a76d612e0c0cae494df3
pesto13 <ricola1304@gmail.com>
2023-07-26 19:20:40 +0200

    es24

--- cpp/max/daOGGI/es24.cpp
+++ cpp/max/daOGGI/es24.cpp
@@ -9,3 +9,71 @@
     se > 30 vero
     falso altrimenti
 */  
+#include<iostream>
+#include<ctime>
+using namespace std;
+void nm(int m[][100], int dimr, int dimc);
+void st(int m[][100], int dimr, int dimc);
+int tr(int m[][100], int dimr, int dimc, int cerca);
+void p(int m[][100], int dimr , int dimc);
+
+
+int main()
+{
+    int m[100][100], dimr, dimc, conta;
+    cout<<"Inserisci numero righe ";
+    cin>>dimr;
+    cout<<"Inserisci numero colonne: ";
+    cin>>dimc;
+
+
+    srand(time(NULL));
+    nm(m, dimr, dimc);
+    st(m, dimr, dimc);
+    p(m, dimr, dimc);
+    
+    
+    return 0;
+
+}
+void nm(int m[][100], int dimr, int dimc){
+    int i, j;
+
+    for(i=0; i<dimr; i++){
+        for(j=0; j<dimc; j++){
+            m[i][j]=rand()%(9-0+1)+0;
+        }
+    }
+}
+void st(int m[][100], int dimr, int dimc){
+    int i, j;
+     for(i=0; i<dimr; i++){
+        for(j=0; j<dimc; j++){
+            cout<<m[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int tr(int m[][100], int dimr, int dimc, int cerca){
+    int conta=0;
+    int i, j;
+    for(i=0; i<dimr; i++){
+        for(j=0; j<dimc; j++){
+            if(m[i][j]==cerca){
+            conta++;
+        }
+    }
+    
+    }
+    return conta;
+}
+void p(int m[][100], int dimr , int dimc){
+    int per, conta;
+    per=dimr*dimc*80/100;
+    conta=tr(m, dimr, dimc, 0);
+    if(conta>=per){
+        cout<<"VERO";
+    }else{
+        cout<<"FALSO";
+    }
+}

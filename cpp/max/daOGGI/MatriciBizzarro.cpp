9c1418645d89ac948e80b1d8628be5a220334295
pesto13 <ricola1304@gmail.com>
2023-08-07 16:54:32 +0200

    aggiunti esercizi

---
+++ cpp/max/daOGGI/MatriciBizzarro.cpp
@@ -0,0 +1,23 @@
+/* 
+
+
+
+Esercizio C++ con Matrici Bizzarro
+
+Scrivi un programma in C++ che esegua le seguenti operazioni:
+
+Chieda all'utente di inserire la dimensione 
+n di una matrice quadrata.
+Crea una matrice 
+n×n
+n×n speciale con la seguente caratteristica:
+Gli elementi sulla diagonale principale sono la somma degli indici di riga e colonna più 1, ovvero 
+a(i,i)=i+j+1.
+Gli elementi fuori dalla diagonale principale sono tutti 0.
+Stampa la matrice speciale appena creata.
+Calcola e stampa il prodotto dei seguenti elementi:
+Il prodotto degli elementi sulla diagonale principale.
+Il prodotto degli elementi sulla diagonale secondaria (dalla parte superiore sinistra all'angolo inferiore destro).
+Assicurati che il programma sia ben strutturato, leggibile e che utilizzi loop appropriati per riempire la matrice e calcolare i prodotti delle diagonali.
+
+ */

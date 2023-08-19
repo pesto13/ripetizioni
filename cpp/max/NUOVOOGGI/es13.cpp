1a2fa634dd7de861158366c076362b144fd7f8fd
pesto13 <ricola1304@gmail.com>
2023-08-19 15:58:43 +0200

    esercizio matrici

--- cpp/max/NUOVOOGGI/es13.cpp
+++ cpp/max/NUOVOOGGI/es13.cpp
@@ -1,4 +1,28 @@
-/* 
+/*
 Leggere una stringa e contare quanto caratteri diversi tra loro contiene.
 
  */
+#include <stdio.h>
+
+int main()
+{
+    char stringa[100];
+    printf("Inserisci una stringa: ");
+    scanf("%s", stringa);
+
+    int caratteriDiversi = 0;
+    int caratteri[256] = {0}; // Assumendo che lavoriamo con caratteri ASCII
+
+    for (int i = 0; stringa[i] != '\0'; i++)
+    {
+        if (caratteri[(int)stringa[i]] == 0)
+        {
+            caratteri[(int)stringa[i]] = 1;
+            caratteriDiversi++;
+        }
+    }
+
+    printf("La stringa contiene %d caratteri diversi.\n", caratteriDiversi);
+
+    return 0;
+}

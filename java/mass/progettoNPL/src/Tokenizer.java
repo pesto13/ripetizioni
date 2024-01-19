4b0361f609c92975ad31f773edf9bb0e721c81e9
pesto13 <ricola1304@gmail.com>
2024-01-19 19:45:44 +0100

    aggiunte mappe ordinate e scritti i TFF

--- java/mass/progettoNPL/src/Tokenizer.java
+++ java/mass/progettoNPL/src/Tokenizer.java
@@ -1,16 +1,23 @@
+import com.sun.source.tree.Tree;
+
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.security.KeyPair;
+import java.util.*;
 import java.util.regex.*;
 public class Tokenizer {
 
     String root = "MATERIE/";
     ArrayList<String> stopWords;
+    ArrayList<Map<String, Integer>> mappaMateria;
+    ArrayList<LinkedHashMap<String, Integer>> sortedMaps;
 
     public Tokenizer(ArrayList<String> stopWords){
         this.stopWords = stopWords;
+        this.mappaMateria = new ArrayList<>();
+        this.sortedMaps = new ArrayList<>();
     }
 
     public void createTFFile(){
@@ -38,12 +45,67 @@ public class Tokenizer {
         File folder = new File(root);
         File[] listOfOutersDirectories = folder.listFiles();
         for(File outerDirectory : listOfOutersDirectories){
-            createSingleMap(outerDirectory);
+            mappaMateria.add(createSingleMap(outerDirectory));
+        }
+
+        //todo
+
+        for(Map m : mappaMateria){
+            List<Map.Entry<String, Integer>> listaVoci = new ArrayList<>(m.entrySet());
+
+            // Ordinamento della lista in base ai valori (integer)
+            Collections.sort(listaVoci, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                    return entry2.getValue().compareTo(entry1.getValue());
+                }
+            });
+
+            // Creazione di una nuova LinkedHashMap per mantenere l'ordine
+            LinkedHashMap<String, Integer> hashMapOrdinata = new LinkedHashMap<>();
+
+            // Aggiunta delle voci ordinate alla nuova LinkedHashMap
+            for (Map.Entry<String, Integer> entry : listaVoci) {
+                hashMapOrdinata.put(entry.getKey(), entry.getValue());
+            }
+            sortedMaps.add(hashMapOrdinata);
+        }
+
+
+        //TODO ordinare mappa
+        int counter = 0;
+        for(File outerDirectory : listOfOutersDirectories){
+            for(File innerFile: outerDirectory.listFiles()){
+                Pattern pattern = Pattern.compile(".*terms_frequency.*");
+                if (pattern.matcher(innerFile.getName()).matches()) {
+                    //System.out.println(counter);
+                    writeOnTermFrequencyFile(innerFile, sortedMaps.get(counter));
+                    counter++;
+                }
+            }
+        }
+    }
+
+    private void writeOnTermFrequencyFile(File innerfile, Map<String, Integer> mappa){
+        try {
+            FileWriter myWriter = new FileWriter(innerfile);
+
+            for (Map.Entry<String, Integer> entry : mappa.entrySet()) {
+                myWriter.write(entry.getKey() + ": " + entry.getValue() + '\n');
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 
 
-    private void createSingleMap(File outerDirectory) {
+    private Map<String, Integer> createSingleMap(File outerDirectory) {
+
+        // qua abbiamo la nostra mappa della singola materia
+        Map<String, Integer> tempmap = new HashMap<>();
+
         for(File singleFile: outerDirectory.listFiles()){
             Pattern pattern = Pattern.compile(".*terms_frequency.*");
             if (!pattern.matcher(singleFile.getName()).matches()) {
@@ -54,10 +116,14 @@ public class Tokenizer {
                     e.printStackTrace();
                 }
                 while (myReader.hasNextLine()) {
-                    String data = myReader.next();
-                    if(!stopWords.contains(data)){
-                        //System.out.println(data);
-                        //TODO fare la mappa<string, int>
+                    String word = myReader.next();
+                    if(!stopWords.contains(word)){
+                        if(tempmap.containsKey(word)){
+                            int value = tempmap.get(word);
+                            tempmap.put(word, value+1);
+                        }else{
+                            tempmap.put(word, 1);
+                        }
                     }
 
                 }
@@ -65,7 +131,7 @@ public class Tokenizer {
             }
         }
 
-        //TODO salva la mappa
+        return tempmap;
     }
 
 

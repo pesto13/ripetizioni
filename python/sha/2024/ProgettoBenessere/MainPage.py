565e546efc11cffe07f520051fd3c5a2cf7c94b0
pesto13 <ricola1304@gmail.com>
2024-04-29 18:56:16 +0200

    doppione

--- python/sha/2024/ProgettoBenessere/MainPage.py
+++ python/sha/2024/ProgettoBenessere/MainPage.py
@@ -60,6 +60,7 @@ def rispondi(felicita_lvl, frase_text):
 
     if risposta == 'Bene' or risposta == 'Male':
         frase = frasi.restituisci_frase(risposta)
+        label_indovina['text'] = frase
         frase_text.config(state=tk.NORMAL)
         frase_text.delete('1.0', tk.END)
         frase_text.insert('1.0', frase)
@@ -70,51 +71,70 @@ def rispondi(felicita_lvl, frase_text):
         frase_text.insert('1.0', 'Non ho capito cosa intendevi')
         frase_text.config(state=tk.DISABLED)
 
-def confirm_action(felicita, frase_text):
-    with open('stato.txt', 'a') as f:
-        f.write(f"{global_username} {felicita}\n")
-    # 
+def confirm_action1():
+    file = open('stato.txt', 'a')
+    file.write(global_username + ' 1' + '\n')
+    file.close()
     frase_text.pack(pady=10, padx=10)
-    rispondi(felicita, frase_text)
+    rispondi(1, frase_text)
 
-def draw_hang(vite):
+def confirm_action3():
+    file = open('stato.txt', 'a')
+    file.write(global_username + ' 3' + '\n')
+    file.close()
+    frase_text.pack(pady=10, padx=10)
+    rispondi(3, frase_text)
+
+def draw_hang(v):
     hang_text.delete('1.0', tk.END)
-    hang_text.insert('1.0', disegni[-vite])
+    hang_text.insert('1.0', disegni[v])
+
+def update_parola(char):
+    for i in range(len(parola_indovinare)):
+        if parola_indovinare[i] == char:
+            hidden_parola[i] = char
+            
+    label_indovina['text'] = hidden_parola
+    
     
 def fguess_char():
-    global vite
+    vite = vite_label['text'].replace(' ', '')
+    vite = int(vite)
     char = entry_char.get()
     if char not in parola_indovinare:
         vite -= 1
+        vite_label['text'] = vite
         draw_hang(vite)
-        return
+        if vite == 0:
+            label_indovina['text'] = 'HAI PERSO'
     else:
-        print("fai robe")
+        update_parola(char)
+        if hidden_parola == parola_indovinare:
+            label_indovina['text'] = 'HAI VINTO'
 
+    
 global_username = 'TODO'
 parola_indovinare = 'ciao'
-vite = 5
+hidden_parola = ['_' for _ in range(len(parola_indovinare))]
+
 finestra = tk.Tk()
 finestra.title("Come ti senti oggi?")
 finestra.geometry("500x900")
 finestra.configure(bg="#03ecfc")
 
-domanda_label = tk.Label(finestra, text="Come ti senti oggi?", font=("Helvetica", 14), bg="#03ecfc")
-domanda_label.pack(pady=10)
+vite_label = tk.Label(finestra, text="Come ti senti oggi?", font=("Helvetica", 14), bg="#03ecfc")
+vite_label.pack(pady=10)
 
-button1 = tk.Button(finestra, text="\U0001F61F", font=("Segoe UI Emoji", 12), command=lambda: confirm_action(1, frase_text))
+button1 = tk.Button(finestra, text="Male", font=("Segoe UI Emoji", 12), command=confirm_action1)
 button1.pack(padx=10)
 
-button2 = tk.Button(finestra, text="\U0001F610", font=("Segoe UI Emoji", 12), command=lambda: confirm_action(2, frase_text))
-button2.pack(padx=10)
-
-button3 = tk.Button(finestra, text="\U0001F604", font=("Segoe UI Emoji", 12), command=lambda: confirm_action(3, frase_text))
+button3 = tk.Button(finestra, text="Bene", font=("Segoe UI Emoji", 12), command=confirm_action3)
 button3.pack(padx=10)
 
 frase_text = tk.Text(finestra, font=("Helvetica", 16), bg="#03ecfc", state=tk.DISABLED)
 frase_text.pack(padx=10)
 
-label_indovina = tk.Label(finestra, text=f'{parola_indovinare}', font=("Helvetica", 16), bg="#03ecfc")
+label_indovina = tk.Label(finestra, text='', font=("Helvetica", 16), bg="#03ecfc")
 label_indovina.pack(padx=10)
 
 entry_char = tk.Entry(finestra, font=("Helvetica", 12))
@@ -122,6 +142,9 @@ entry_char.pack(padx=10)
 
 guess_char = tk.Button(finestra, text="Indovina Lettera", font=("Helvetica", 12), command=fguess_char)
 guess_char.pack(padx=10)
+
+vite_label = tk.Label(finestra, text=5, font=("Helvetica", 14), bg="#03ecfc")
+vite_label.pack(pady=10)
 
 hang_text = tk.Text(finestra, font=("Helvetica", 16), bg="#03ecfc")
 hang_text.pack(padx=10)

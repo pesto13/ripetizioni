89680124bb4674dd8f068fc62628d8ff2d147b0f
pesto13 <ricola1304@gmail.com>
2024-04-29 19:14:52 +0200

    entry

--- python/sha/2024/ProgettoBenessere/MainPage.py
+++ python/sha/2024/ProgettoBenessere/MainPage.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import frasi
+import random
 
 disegni = [
     """
@@ -57,33 +58,29 @@ def rispondi(felicita_lvl, frase_text):
         risposta = 'Male'
     else:
         risposta = 'Bene'
-
-    if risposta == 'Bene' or risposta == 'Male':
-        frase = frasi.restituisci_frase(risposta)
-        label_indovina['text'] = frase
-        frase_text.config(state=tk.NORMAL)
-        frase_text.delete('1.0', tk.END)
-        frase_text.insert('1.0', frase)
-        frase_text.config(state=tk.DISABLED)
-    else:
-        frase_text.config(state=tk.NORMAL)
-        frase_text.delete('1.0', tk.END)
-        frase_text.insert('1.0', 'Non ho capito cosa intendevi')
-        frase_text.config(state=tk.DISABLED)
-
+    
+    
+    frase = frasi.restituisci_frase(risposta)
+    
+    frase = list(frase)
+    random.shuffle(frase)
+    f = ''.join(frase)
+    label_chiaro['text'] = frase
+    label_storto['text'] = f
+    label_hidden['text'] = ['_' for _ in range(len(f))]
+    
+    
 def confirm_action1():
     file = open('stato.txt', 'a')
     file.write(global_username + ' 1' + '\n')
     file.close()
-    frase_text.pack(pady=10, padx=10)
-    rispondi(1, frase_text)
+    rispondi(1, '')
 
 def confirm_action3():
     file = open('stato.txt', 'a')
     file.write(global_username + ' 3' + '\n')
     file.close()
-    frase_text.pack(pady=10, padx=10)
-    rispondi(3, frase_text)
+    rispondi(3, '')
 
 def draw_hang(v):
     hang_text.delete('1.0', tk.END)
@@ -94,8 +91,7 @@ def update_parola(char):
         if parola_indovinare[i] == char:
             hidden_parola[i] = char
             
-    label_indovina['text'] = hidden_parola
-    
+    label_storto['text'] = hidden_parola
     
 def fguess_char():
     vite = vite_label['text'].replace(' ', '')
@@ -106,16 +102,15 @@ def fguess_char():
         vite_label['text'] = vite
         draw_hang(vite)
         if vite == 0:
-            label_indovina['text'] = 'HAI PERSO'
+            label_storto['text'] = 'HAI PERSO'
     else:
         update_parola(char)
-        if hidden_parola == parola_indovinare:
-            label_indovina['text'] = 'HAI VINTO'
+        if label_hidden['text'] == label_chiaro['text']:
+            label_storto['text'] = 'HAI VINTO'
 
     
 global_username = 'TODO'
 parola_indovinare = 'ciao'
-hidden_parola = ['_' for _ in range(len(parola_indovinare))]
 
 finestra = tk.Tk()
 finestra.title("Come ti senti oggi?")
@@ -131,11 +126,14 @@ button1.pack(padx=10)
 button3 = tk.Button(finestra, text="Bene", font=("Segoe UI Emoji", 12), command=confirm_action3)
 button3.pack(padx=10)
 
-frase_text = tk.Text(finestra, font=("Helvetica", 16), bg="#03ecfc", state=tk.DISABLED)
-frase_text.pack(padx=10)
+label_chiaro = tk.Label(finestra, text='', font=("Helvetica", 16), bg="#03ecfc")
+# label_chiaro.pack(padx=10)
 
-label_indovina = tk.Label(finestra, text='', font=("Helvetica", 16), bg="#03ecfc")
-label_indovina.pack(padx=10)
+label_storto = tk.Label(finestra, text='', font=("Helvetica", 16), bg="#03ecfc")
+label_storto.pack(padx=10)
+
+label_hidden = tk.Label(finestra, text='', font=("Helvetica", 16), bg="#03ecfc")
+label_hidden.pack(padx=10)
 
 entry_char = tk.Entry(finestra, font=("Helvetica", 12))
 entry_char.pack(padx=10)
